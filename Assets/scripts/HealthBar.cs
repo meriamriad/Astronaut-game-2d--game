993cce67e4d269219afa6f167ceff48e8376cf26
meriamriad <142324728+meriamriad@users.noreply.github.com>
2024-06-04 15:10:53 +0300

    Add files via upload

---
+++ Assets/scripts/HealthBar.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private RectTransform bar;
+    private Image barImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar = GetComponent<RectTransform>();
+        barImage = GetComponent<Image>();
+        if (Health.totalHealth < 0.3f)
+        {
+            barImage.color = Color.red;
+        }
+        SetSize(Health.totalHealth);
+    }
+
+    public void Damage(float damage)
+    {
+        if((Health.totalHealth -= damage) >= 0f)
+        {
+            Health.totalHealth -= damage;
+        }
+        else
+        {
+            Health.totalHealth = 0f;
+            SceneManager.LoadScene(0);
+            Health.totalHealth=1;
+
+        }
+
+        if(Health.totalHealth < 0.3f)
+        {
+            barImage.color = Color.red;
+        }
+       
+
+        SetSize(Health.totalHealth);
+    }
+
+    public void SetSize(float size)
+    {
+        bar.localScale = new Vector3(size, 1f);
+    }
+}
